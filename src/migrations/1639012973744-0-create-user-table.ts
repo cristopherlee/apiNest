6870af957dffeb703ccf95631f9ab3b2c847046b
cristopherlee <cristopherlee@gmail.com>
2021-12-22 23:04:18 -0300

    chore: pipeline e melhorias

--- src/migrations/1639012973744-0-create-user-table.ts
+++ src/migrations/1639012973744-0-create-user-table.ts
@@ -4,6 +4,14 @@ export class createUserTable1639012973744 implements MigrationInterface {
   name = 'createUserTable1639012973744';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    /* TODO: Testing creating the databases
+    await queryRunner.query(`DROP DATABASE IF EXISTS test`);
+    await queryRunner.query(`CREATE DATABASE test`);
+
+    await queryRunner.query(`DROP DATABASE IF EXISTS e2e_test`);
+    await queryRunner.query(`CREATE DATABASE e2e_test`);
+    */
+
     // this ensure we can use default: `uuid_generate_v4()`
     await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
 
@@ -40,7 +48,7 @@ export class createUserTable1639012973744 implements MigrationInterface {
             default: 'CURRENT_TIMESTAMP',
           },
         ],
-      }),
+      })
     );
   }
 
